ef86f1b6ab712e6bf487860ff333f8b2548161bf
JayaChandra Battula <39637291+JayaChandraBathula@users.noreply.github.com>
2018-10-04 10:00:36 -0400

    Delete postgresconfig.py

--- database/postgresconfig.py
+++
@@ -1,6 +0,0 @@
-DB Instance Identifier: rds-postgresinstance
-master username: chandra
-master password: Searchfunction
-database name: mypostgresdb
-port: 5432
-host="rds-postgresinstance.c5cn8wdvuzrw.us-east-1.rds.amazonaws.com"
