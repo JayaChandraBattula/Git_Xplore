fbfe4bd8a3b160e138e441d2e1ac24f646e62b18
Chandra <jayachandrabattula@gmail.com>
2018-10-11 01:00:22 -0400

    commit 65-psycopg2 cur code

--- src/app/webpage.py
+++ src/app/webpage.py
@@ -1,8 +1,25 @@
 from flask import render_template, request, flash, url_for, redirect
 from flask import Flask
+import psycopg2
+import psycopg2.extras
 
 
 app = Flask('Git Xplore')
+
+# parameters for postgres database connection
+params = {
+    'database': 'mypostgresdb',
+    'user': 'chandra',
+    'password': 'Searchfunction',
+    'host': 'rds-postgresinstance.c5cn8wdvuzrw.us-east-1.rds.amazonaws.com',
+    'port': 5432
+}
+
+try:
+    conn = psycopg2.connect(**params)
+except Exception as er:
+        print("Unable to connect to the database")
+        print(str(er))
 
 
 @app.route("/")
@@ -10,6 +27,37 @@ app = Flask('Git Xplore')
 def home():
     title = 'Home'
     return render_template('home.html', title=title)
+
+
+@app.route("/getdata", methods=["GET", "POST"])
+def getdata():
+    repoid = request.args.get('repo_id')
+    reponame = request.args.get('repo_name')
+    print("repo name ",reponame)
+    classname = request.args.get('class_name')
+    print("class name ",classname)
+    methodnames = request.args.get('method_names')
+
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    if (reponame != none and classname!=none):
+        likeString1 = "'%%" + reponame + "%%'"
+        print("likeString1 ",likeString1)
+        likeString2 = "'%%" + classname + "%%'"
+        print("likeString2 ",likeString2)
+        # get data from table 'total'
+        cur.execute("SELECT *\
+                       FROM total \
+                      WHERE repo_name like %s AND class_name like %s;",\
+                       (likeString1, likeString2))
+    resultst=cur.fetchall()
+    return render_template('view.html', repoinfo=results)
+
+
+
+@app.route("/about")
+def about():
+    title = 'About'
+    return render_template('about.html', title=title)
 
 if (__name__ == "__main__"):
     app.run(host='ec2-52-6-184-3.compute-1.amazonaws.com',debug='true')
